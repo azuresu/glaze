61c6eeb9c51acd4f044fb452b8720353161c102c
Azure Su <qqsu.azure@gmail.com>
2025-12-03 11:47:04 +0800

    support varargs in log

--- addons/glaze/glaze.gd
+++ addons/glaze/glaze.gd
@@ -32,12 +32,13 @@ func new_scene(scene_name: String, parent: Node = null) -> Node:
 				scene_path = data["scene_path"]
 		if not scene_path:
 			scene_path = scene_name
-			log_warn("Scene path is not defined with scene name: %s" % scene_name)
+			if not scene_path.ends_with(".tscn"):
+				log_warn("Scene path is not defined and scene name: %s does not end with .tscn", scene_name)
 		var s:= load(scene_path)
 		if s:
 			_packed_scenes[scene_name] = s
 		else:
-			log_error("Scene not found: %s" % scene_path)
+			log_error("Scene not found: %s", scene_path)
 	if scene_name in _packed_scenes:
 		var scene: Node = _packed_scenes[scene_name].instantiate()
 		# Copy data into scene.
@@ -92,34 +93,40 @@ func log_debug_enabled() -> bool:
 	return log_level == LogLevel.DEBUG
 
 ## Logs a debug-level message.
-func log_debug(message: String) -> void:
-	return log_msg(LogLevel.DEBUG, message)
+func log_debug(message: String, ...args) -> void:
+	return log_msg(LogLevel.DEBUG, message, args)
 
 ## Logs an info-level message.
-func log_info(message: String) -> void:
-	return log_msg(LogLevel.INFO, message)
+func log_info(message: String, ...args) -> void:
+	return log_msg(LogLevel.INFO, message, args)
 
 ## Logs a warn-level message.
-func log_warn(message: String) -> void:
-	return log_msg(LogLevel.WARN, message)
+func log_warn(message: String, ...args) -> void:
+	return log_msg(LogLevel.WARN, message, args)
 
 ## Logs an error-level message.
-func log_error(message: String) -> void:
-	return log_msg(LogLevel.ERROR, message)
+func log_error(message: String, ...args) -> void:
+	return log_msg(LogLevel.ERROR, message, args)
 
 ## Logs a fatal-level message.
-func log_fatal(message: String) -> void:
-	return log_msg(LogLevel.FATAL, message)
+func log_fatal(message: String, ...args) -> void:
+	return log_msg(LogLevel.FATAL, message, args)
 
 ## Logs a message with specified level.
-func log_msg(level: LogLevel, message: String) -> void:
+func log_msg(level: LogLevel, message: String, ...args) -> void:
 	if level >= log_level:
 		var time = Time.get_datetime_dict_from_system()
 		var time_str:= "%04d-%02d-%02d %02d:%02d:%02d" % [time.year, time.month, time.day, time.hour, time.minute, time.second]
+		var msg_str:= message
+		# Varargs does not support unpacking in 4.5.
+		if args.size() > 1:
+			msg_str = message % args
+		elif args.size() > 0:
+			msg_str = message % args[0]
 		if log_rich_text:
-			print_rich("[color=%s]%s | %s | %s[/color]" % [_LOG_LEVEL_COLORS[level], time_str, _LOG_LEVEL_NAMES[level], message])
+			print_rich("[color=%s]%s | %s | %s[/color]" % [_LOG_LEVEL_COLORS[level], time_str, _LOG_LEVEL_NAMES[level], msg_str])
 		else:
-			print("%s | %s | %s" % [time_str, _LOG_LEVEL_NAMES[level], message])
+			print("%s | %s | %s" % [time_str, _LOG_LEVEL_NAMES[level], msg_str])
 
 ## Adds built-in parser.
 ## Added parser is always inserted to the first position in parser queue therefore it has chance to override default parsers.
@@ -169,11 +176,11 @@ func load_json_as_dict(filename: String, parse_builtin_types:= false) -> Diction
 					parse_builtin_types(json.data)
 				return json.data
 			else:
-				log_error("JSON is not a dictionary in json file: %s" % filename)
+				log_error("JSON is not a dictionary in json file: %s", filename)
 		else:
-			log_error("Error in parsing json file: %s" % filename)
+			log_error("Error in parsing json file: %s", filename)
 	else:
-		log_error("File not found: %s" % filename)
+		log_error("File not found: %s", filename)
 	return {}
 
 ## Loads JSON as array from file.
@@ -188,15 +195,15 @@ func load_json_as_array(filename: String, parse_builtin_types:= false) -> Array:
 					parse_builtin_types(json.data)
 				return json.data
 			else:
-				log_error("JSON is not an array in json file: %s" % filename)
+				log_error("JSON is not an array in json file: %s", filename)
 		else:
-			log_error("Error in parsing json file: %s" % filename)
+			log_error("Error in parsing json file: %s", filename)
 	else:
-		log_error("File not found: %s" % filename)
+		log_error("File not found: %s", filename)
 	return []
 
 func reload() -> void:
-	log_info("Reloading configuration: %s" % _CONFIG_FILE)
+	log_info("Reloading configuration: %s", _CONFIG_FILE)
 	# Reset to default.
 	log_level = LogLevel.INFO
 	log_rich_text = true
@@ -214,7 +221,7 @@ func reload() -> void:
 			if log_level >= 0:
 				self.log_level = log_level
 			else:
-				log_error("Invalid log level: %s" % config["log_level"])
+				log_error("Invalid log level: %s", config["log_level"])
 
 		if "log_rich_text" in config:
 			log_rich_text = config["log_rich_text"] as bool
@@ -226,20 +233,20 @@ func reload() -> void:
 			scene_data_files = config["scene_data_files"] as Array
 
 		for file in scene_data_files:
-			log_debug("Loading scene data file: %s" % file)
+			log_debug("Loading scene data file: %s", file)
 			var data:= load_json_as_dict(file, scene_data_allow_builtin_types)
 			for scene_name in data:
 				if scene_name in scene_data:
 					scene_data[scene_name].merge(data[scene_name], true)
-					log_debug("Scene data merged: %s" % scene_name)
+					log_debug("Scene data merged: %s", scene_name)
 				else:
 					scene_data[scene_name] = data[scene_name]
-					log_debug("Scene data added: %s" % scene_name)
+					log_debug("Scene data added: %s", scene_name)
 		for scene_name in scene_data:
 			_merge_derived_data(scene_name)
-		log_info("Loaded scenes: %s" % scene_data.size())
+		log_info("Loaded scenes: %s", scene_data.size())
 	else:
-		log_warn("Missing configuration file: %s" % _CONFIG_FILE)
+		log_warn("Missing configuration file: %s", _CONFIG_FILE)
 
 func _ready() -> void:
 	_builtin_parsers.append_array(Parser.builtin_parsers)
@@ -250,9 +257,9 @@ func _merge_derived_data(scene_name: String, derived_chain:= {}) -> Dictionary:
 		var data:= scene_data[scene_name] as Dictionary
 		if "derived_scene" in data:
 			var derived_scene_name:= data["derived_scene"] as String
-			log_debug("Found scene: %s derived from scene: %s" % [scene_name, derived_scene_name])
+			log_debug("Found scene: %s derived from scene: %s", scene_name, derived_scene_name)
 			if derived_scene_name in derived_chain:
-				log_error("Looping derived scene: %s" % derived_scene_name)
+				log_error("Looping derived scene: %s", derived_scene_name)
 			else:
 				derived_chain[derived_scene_name] = 1
 				var derived_data:= _merge_derived_data(data["derived_scene"], derived_chain)
@@ -261,5 +268,5 @@ func _merge_derived_data(scene_name: String, derived_chain:= {}) -> Dictionary:
 						data[k] = derived_data[k]
 			data.erase("derived_scene")
 		return data
-	log_error("Missing derived scene: %s" % scene_name)
+	log_error("Missing derived scene: %s", scene_name)
 	return {}
