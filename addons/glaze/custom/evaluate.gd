a0d940e4e2c9834fb9402be38860fbcb63c66b91
Azure Su <qqsu.azure@gmail.com>
2026-02-04 09:58:50 +0800

    evaluate will fall back to owner if source is not specified

--- addons/glaze/custom/evaluate.gd
+++ addons/glaze/custom/evaluate.gd
@@ -3,7 +3,7 @@
 ## Name this node with the property name in the parent node.
 class_name Evaluate extends Node
 
-## Source of evaluation
+## Source of evaluation. Fall back to owner if not specified.
 @export var source: Node
 ## Name of the variable or function in the source.
 @export var source_var: String
@@ -11,13 +11,21 @@ class_name Evaluate extends Node
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		return
-	if source and source_var:
-		if name in get_parent():
-			if source.has_method(source_var):
-				get_parent()[name] = source.call(source_var)
-			elif source_var in source:
-				get_parent()[name] = source[source_var]
-			else:
-				Glaze.log_error("No such variable: %s on source: %s", source_var, source)
+
+	var s:= source
+	if not s:
+		s = owner
+
+	if not s:
+		Glaze.log_error("Source not found when evaluating: %s", name)
+	elif not source_var:
+		Glaze.log_error("Source variable found when evaluating: %s", name)
+	elif name in get_parent():
+		if s.has_method(source_var):
+			get_parent()[name] = s.call(source_var)
+		elif source_var in s:
+			get_parent()[name] = s[source_var]
 		else:
-			Glaze.log_error("No such property: %s on %s", name, get_parent())
+			Glaze.log_error("No such variable: %s on source: %s", source_var, s)
+	else:
+		Glaze.log_error("No such property: %s on %s", name, get_parent())
