cd4678089d37d46d04c43721e3c902fac1323a9d
Azure Su <qqsu.azure@gmail.com>
2026-02-09 10:24:57 +0800

    Update quick_button.gd

--- addons/glaze/custom/quick_button.gd
+++ addons/glaze/custom/quick_button.gd
@@ -1,14 +1,21 @@
 @icon("res://addons/glaze/custom/quick_button.png")
-extends Button
+class_name QuickButton extends Button
 
 @export var func_owner: Node
+@export var func_name: String
 @export var func_params: Array
 
 func _on_pressed() -> void:
 	var fo:= func_owner
 	if not fo:
 		fo = owner
-	if fo.has_method(name):
-		fo.callv(name, func_params)
+	if fo:
+		var fn:= func_name
+		if not fn:
+			fn = name
+		if fo.has_method(fn):
+			fo.callv(fn, func_params)
+		else:
+			Glaze.log_error("Func name: %s not found on owner:%s", fn, fo)
 	else:
 		Glaze.log_error("Func owner not found: %s", self)
