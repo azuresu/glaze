d909d43cb17190577fe87991756f0b571308fa48
Azure Su <qqsu.azure@gmail.com>
2026-05-16 10:30:09 +0800

    rename _transition() to transition()

--- addons/glaze/custom/state.gd
+++ addons/glaze/custom/state.gd
@@ -10,7 +10,7 @@ var machine: StateMachine:
 
 var update_time: float
 
-func _transition(state_name: String, params:= {}) -> void:
+func transition(state_name: String, params:= {}) -> void:
 	machine.set_current_state(state_name, params)
 
 func _enter() -> void:
